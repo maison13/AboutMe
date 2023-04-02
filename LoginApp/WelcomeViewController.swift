6f27d269996551e0464cd00fb462355d807a6df4
Vladimir <smile_friend@mail.ru>
2023-04-02 21:10:03 +0300

    Setup WelcomeViewController

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Vovo on 02.04.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var userLabel: UILabel!
+    
+    var userTextLabel: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userLabel.text = userTextLabel
+       
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.systemPurple.cgColor, UIColor.systemTeal.cgColor]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+
+    
+
+}
