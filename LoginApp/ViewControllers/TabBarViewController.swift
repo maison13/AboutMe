4e032325574b64cf8c300c19fe5921da2d806dba
Vladimir <smile_friend@mail.ru>
2023-04-06 02:38:22 +0300

    setup

--- LoginApp/ViewControllers/TabBarViewController.swift
+++ LoginApp/ViewControllers/TabBarViewController.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-class TabBarViewController: UITabBarController {
+final class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance    
     }
-    
-
-
-
+   
 }
