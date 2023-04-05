047eab21008493d480d95d4884a2e648fb43dc3e
Vladimir <smile_friend@mail.ru>
2023-04-06 00:25:02 +0300

    setup

---
+++ LoginApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBraViewController.swift
+//  LoginApp
+//
+//  Created by Vovo on 05.04.2023.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
