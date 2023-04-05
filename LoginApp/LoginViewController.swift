26c284fd7f680340868655900378e5c6ba09ef60
Vladimir <smile_friend@mail.ru>
2023-04-05 21:39:44 +0300

    Setup LoginVC

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -13,6 +13,9 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
+    private let login = "Guest"
+    private let password = "Pass"
+    
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +24,13 @@ class LoginViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
-        welcomeVC.userTextLabel = loginTF.text
+        welcomeVC.userTextLabel = login
     }
     // MARK: - IB Actions
     @IBAction func unwind(for segue: UIStoryboardSegue) {
@@ -34,39 +38,30 @@ class LoginViewController: UIViewController {
         passwordTF.text = ""
     }
     
-    @IBAction func forgotNameTapped() {
-        showAlert(
-            withTitle: "Oops",
-            andMessage: "Your  name is Guest \u{1F609}"
-        )
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(withTitle: "Oops", andMessage: "Your  name is Guest \u{1F609}")
+            : showAlert(withTitle: "Oops", andMessage: "Your  password is Pass \u{1F60B}")
     }
-    
-    @IBAction func forgotPasswordTapped() {
-        showAlert(
-            withTitle: "Oops",
-            andMessage: "Your  password is Pass \u{1F60B}"
-        )
-    }
+
     
     @IBAction func loginButtonTapped() {
-        if loginTF.text != "Guest" || passwordTF.text != "Pass" {
+        guard loginTF.text == login, passwordTF.text == password else {
             showAlert(
                 withTitle: "Login or password is not correct",
-                andMessage: "Try logging in again"
+                andMessage: "Try logging in again",
+                textField: passwordTF
             )
+            return
         }
+        performSegue(withIdentifier: "showWelcomVC", sender: nil)
     }
     // MARK: - Private Methods
-    private func showAlert(withTitle title: String, andMessage message: String) {
+    private func showAlert(withTitle title: String, andMessage message: String, textField: UITextField? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAktion = UIAlertAction(title: "ok", style: .default) { _ in
-            switch message {
-            case "Your  name is Guest \u{1F609}":
-                self.loginTF.text = ""
-            default:
-                self.passwordTF.text = ""
+            textField?.text = ""
             }
-        }
         alert.addAction(okAktion)
         present(alert, animated: true)
     }
